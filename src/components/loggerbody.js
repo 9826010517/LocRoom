ccb52fa510b3d3d61ee6475b67986c91dbb68011
unknown <suchit0011@gmail.com>
2020-10-13 20:05:56 +0530

    chat board added

--- src/components/loggerbody.js
+++ src/components/loggerbody.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Form } from 'react-bootstrap';
 import { Tabs,Tab } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 import { Dropdown } from 'react-bootstrap';
+import $ from 'jquery';
 export class Loggerbody extends React.Component {
     state = {
          arr : [{"id":"1","name":"amit mishra","status":"i am good","status":"i am good","time":"10am","img":"assets/adminsection/dist/img/user2-160x160.jpg"},
@@ -16,16 +18,40 @@ export class Loggerbody extends React.Component {
         this.setState({ selected: id });
         console.log('new section',id)
       };
-
+    // For open close right nav bar  
+     openNav = () => {
+         alert('open nav')
+        document.getElementById("mySidenav").style.width = "250px";
+      }
+      
+       closeNav = () => {
+        alert('close nav')
+        document.getElementById("mySidenav").style.width = "0";
+      }
+      
+    // 
       render() {
+        $(document).ready(function(){
+            $(".slideBtn").click(function(){    
+              if($("#sidenav").width() == 0){      
+                  document.getElementById("sidenav").style.width = "250px";
+                  document.getElementById("main").style.paddingRight = "250px";
+                //   document.getElementById("slidebtn").style.paddingRight = "250px";
+                  document.getElementById("slidebtn").style.paddingRight = "0px";
+              }else{
+                  document.getElementById("sidenav").style.width = "0";
+                  document.getElementById("main").style.paddingRight = "0";
+                  document.getElementById("slidebtn").style.paddingRight = "0";
+              }
+            });
+          });
         const { selected, arr } = this.state;
-        var btn_class = selected === "" ? "blackButton" : "whiteButton";
         console.log(selected);
         return (
             <div className="content-wrapper">
               {/* ****************** */}
               <div className="conversion_body">
-                  <div className="row p-4" >
+                  <div className="row p-4 conversion_bodyinner" >
                       <div className="col-3 conversion_body1">
                       <div className="row">
                           <div className="col-6 my-auto pl-3" ><h5>Conversations</h5></div>
@@ -113,13 +139,159 @@ export class Loggerbody extends React.Component {
                       </div>
                       </div>
                       <div className="col-9 conversion_body2 text-center">
-                          <div class="row h-100">
-                              <div class="col-12 d-flex justify-content-center align-items-center">
-                              <h6>No Conversion Yet</h6>
-                              </div>
-                               </div>
-                         
+                          {/* left side bar bio detail*/}
+                          <div id="sidenav" className="sidenav">
+                                <div className="row">
+                                    <div className="col-12 sidenavinner1">
+                                        <h4 className="sidenav_abouttag">About</h4>
+                                        <img src="assets/adminsection/dist/img/user2-160x160.jpg" className="img-circle sidenavImg"></img>
+                                        <p className="sidenav_status"><i className="fa fa-circle mr-1 sidenav_statusdot" aria-hidden="true"></i>Online</p>
+                                    </div>
+                                    <div className="col-12 sidenavinner1">
+                                        <h6 className="sidenav_bioheading">Bio</h6>
+                                        <p className="sidenav_biotext">No bio added yet...</p>
+                                    </div>
+                                    <div className="col-12 sidenavinner1">
+                                        <h6 className="sidenav_bioheading">Phone</h6>
+                                        <p className="sidenav_biotext">No phone added yet...</p>
+                                    </div>
+                                    <div className="col-12 sidenavinner1">
+                                        <h6 className="sidenav_bioheading">Email</h6>
+                                        <p className="sidenav_biotext">locroom@gmail.com</p>
+                                    </div>
+                                    <div className="col-12 sidenavinner1">
+                                        <h6 className="sidenav_bioheading">Media</h6>
+                                        <p className="sidenav_biotext">No media shared yet...</p>
+                                    </div>
+                                    <div className="col-12 sidenavinner1">
+                                        <h6 className="sidenav_bioheading">Common Groups - <span>1</span></h6>
+                                        <p className="sidenav_biotext"><i class="fa fa-users mr-3" aria-hidden="true"></i><span>test group</span></p>
+                                        <p className="sidenav_biotext"><i class="fa fa-users mr-3" aria-hidden="true"></i><span>test group</span></p>
+                                    </div>
+                                </div>
+                         </div>
+                          {/*Center body */}
+                                 <div id="main">
+                                 <section className="sidenav_bodysection1 pt-2 pb-2">
+                                 {/* <div id="slidebtn" className="slideBtn">&#9776;</div>   */}
+                                 <div className="row">
+                                 <div className="col-8 text-left pt-2 pl-2">
+                                  <div className="activeuser">
+                                    <img src="assets/adminsection/dist/img/user2-160x160.jpg" className="img-circle activeuserimg"></img>
+                                  </div>
+                                 <div  className="activeuser_detail">
+                                    <h6 className="activeusername">amit</h6>
+                                    <p className="activeuser_tagline">online</p>
+                                 </div>
+                                    </div>
+                                    <div className="col-4 text-right my-auto">
+                                        <h6  id="slidebtn" className="slideBtn"><i class="fa fa-cog" aria-hidden="true"></i></h6>
+                                    </div>
+                                 </div>
+                             </section>
+                             <section className="">
+                             <ul id="chat">
+                                        <li class="you">
+                                        <div class="entete">
+                                            <span class="status green"></span>
+                                            <h2>Vincent</h2>
+                                            <h3>10:12AM, Today</h3>
+                                        </div>
+                                        <div class="triangle"></div>
+                                        <div class="message">
+                                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
+                                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
+                                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
+                                        </div>
+                                    </li>
+                                    <li class="you">
+                                        <div class="entete">
+                                            <span class="status green"></span>
+                                            <h2>Vincent</h2>
+                                            <h3>10:12AM, Today</h3>
+                                        </div>
+                                        <div class="triangle"></div>
+                                        <div class="message">
+                                            Lorem.
+                                        </div>
+                                    </li>
+                                    <li class="me">
+                                        <div class="entete">
+                                            <h3>10:12AM, Today</h3>
+                                            <h2>Vincent</h2>
+                                            <span class="status blue"></span>
+                                        </div>
+                                        <div class="triangle"></div>
+                                        <div class="message">
+                                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
+                                        </div>
+                                    </li>
+                                    <li class="me">
+                                        <div class="entete">
+                                            <h3>10:12AM, Today</h3>
+                                            <h2>Vincent</h2>
+                                            <span class="status blue"></span>
+                                        </div>
+                                        <div class="triangle"></div>
+                                        <div class="message">
+                                            OK
+                                        </div>
+                                    </li>
+                                    <li class="you">
+                                        <div class="entete">
+                                            <span class="status green"></span>
+                                            <h2>Vincent</h2>
+                                            <h3>10:12AM, Today</h3>
+                                        </div>
+                                        <div class="triangle"></div>
+                                        <div class="message">
+                                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
+                                        </div>
+                                    </li>
+                                    <li class="me">
+                                        <div class="entete">
+                                            <h3>10:12AM, Today</h3>
+                                            <h2>Vincent</h2>
+                                            <span class="status blue"></span>
+                                        </div>
+                                        <div class="triangle"></div>
+                                        <div class="message">
+                                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
+                                        </div>
+                                    </li>
+                                    <li class="me">
+                                        <div class="entete">
+                                            <h3>10:12AM, Today</h3>
+                                            <h2>Vincent</h2>
+                                            <span class="status blue"></span>
+                                        </div>
+                                        <div class="triangle"></div>
+                                        <div class="message">
+                                            OK
+                                        </div>
+                                    </li>
+                                    </ul>
+                                      
+                             </section>
+                                <section className="sidenavbot_footer">
+                                       <div >
+                                       <Form className="d-flex">
+                                            <Form.Group controlId="inputtextchat" className="typetextbox1 flex-fill">
+                                                <Form.Control type="text" placeholder="Enter text" className="textboxinput" />
+                                            </Form.Group>
+                                            <Button variant="" type="submit" className="sendbtn">
+                                            <i class="fa fa-paperclip " aria-hidden="true"></i>
+                                           </Button>
+                                            <Button variant="" type="submit" className="sendbtn">
+                                            <i class="fa fa-paper-plane " aria-hidden="true"></i>
+                                            </Button>
+                                         </Form>
+                                        
+                                       </div>
+                             </section>
                            </div>
+                          {/* testing */}   
+                     </div>
                   </div>
               </div>
               {/* **************** */}
